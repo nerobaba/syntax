371c0692cc86286cea8688a921bbd4daa7e3934e
hakju <gkrwn0308@gmail.com>
2023-04-15 11:47:28 +0900

    fix:syntax-next

--- Frontend/NextJs/TailwindCSS적용/app1/pages/index.js
+++ Frontend/NextJs/TailwindCSS적용/app1/pages/index.js
@@ -1,26 +1,18 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Layout from "../components/layout";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import Hero from "@/components/hero";
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <Layout>
-      <Header>
+    <div>
+      <Head>
         <title>빡코딩 포트폴리오</title>
         <meta name="description" content="오늘도 빡코딩!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Header>
-      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <Hero />
-        </div>
-      </section>
-    </Layout>
-  );
+        <link rel='icon' href="/favicon.ico" />
+      </Head>
+      <h1>안녕</h1>
+    </div>
+  )
 }
